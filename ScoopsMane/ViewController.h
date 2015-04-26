a7f493341b988848526f57277b216c0403591291
Jose Manuel Franco <manezgz@hotmail.com>
2015-04-26 19:03:09 +0200

    Initial Commit

---
+++ ScoopsMane/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScoopsMane
+//
+//  Created by Jose Manuel Franco on 26/4/15.
+//  Copyright (c) 2015 Jose Manuel Franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScoopsMane/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScoopsMane
+//
+//  Created by Jose Manuel Franco on 26/4/15.
+//  Copyright (c) 2015 Jose Manuel Franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
